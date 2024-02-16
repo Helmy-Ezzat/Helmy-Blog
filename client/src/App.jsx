c801cc2bdbeee3165bd2efbeecceecec7a589157
Helmy Ezzat <hellmyezzat@gmail.com>
2024-02-16 06:00:00 +0200

    install react js and tailwind css and create the first template

---
+++ client/src/App.jsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="flex justify-center items-center h-svh bg-slate-300">
+      <h1 className="text-slate-700 font-bold text-6xl">Helmy`s Blog</h1>
+    </div>
+  )
+}
+
+export default App
